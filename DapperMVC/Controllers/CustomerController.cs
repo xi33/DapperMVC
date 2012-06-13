db030fc37f46c84aa9f9211ace624afe722b1d44
Pieux Xi <pieuxxi@gmail.com>
2012-06-13 14:15:30 +0800

    add select function using dapper

---
+++ DapperMVC/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DapperMVC.Models;
+
+namespace DapperMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {
+            var customEntities = new CustomerDB();
+            return View(customEntities.GetCustomers());
+        }
+
+    }
+}
---
+++ DapperMVC/Models/CustomerDB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace DapperMVC.Models
+{
+    public class CustomerDB
+    {
+        //public string Connectionstring = @"Data Source=DotNetJalps\SQLExpress;Initial Catalog=CodeBase;Integrated Security=True";
+        public string Connectionstring = 
+                    @"Data Source=.\sqlexpress;Initial Catalog=DapperMVC;Integrated Security=True";
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            using (System.Data.SqlClient.SqlConnection sqlConnection = new System.Data.SqlClient.SqlConnection(Connectionstring))
+            {
+                sqlConnection.Open();
+                var customer = sqlConnection.Query<Customer>("Select * from Customer");
+                return customer;
+
+            }
+        }
+
+    }
+}
